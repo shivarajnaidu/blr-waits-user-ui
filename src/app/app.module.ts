91c89f287454dd79ace94fae7bd09fc4905c17fa
yuvaraj <shivarajnaidu@gmail.com>
2021-05-07 20:02:33 +0530

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppShellModule } from './components/app-shell/app-shell.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppShellModule } from './components/app-shell/app-shell.module';
     BrowserModule,
     AppRoutingModule,
     AppShellModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
